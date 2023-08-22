decd1d5f628192a97e5db9ead668eebca39bd332
Nacho Miranda <nachomiranda@live.com.ar>
2023-08-22 18:45:45 -0300

    Manejo de Archivos Js, implementacion de fs para segundo desafio

--- ProductManager.js
+++ ProductManager.js
@@ -1,26 +1,16 @@
+const fs = require("fs");
+
 class ProductManager {
-  products;
-  static ultimoId = 0;
-  constructor() {
-    this.products = [];
+  constructor(path) {
+    this.path = path;
   }
 
   addProduct({ title, description, price, thumbnail, code, stock = 15 }) {
-    ProductManager.ultimoId++;
-    const id = ProductManager.ultimoId;
+    const products = this.getProductsFromFile();
+    const id = products.length + 1;
 
-    const isExist = this.isExist("code", code);
+    const isExist = products.find((product) => product.code === code);
     if (isExist) throw new Error(`El código ingresado ya existe`);
-
-    if (
-      title.length === 0 ||
-      description.length === 0 ||
-      price.length === 0 ||
-      thumbnail.length === 0 ||
-      code.length === 0 ||
-      stock.length === 0
-    )
-      throw new Error("Todos los campos son obligatorios");
 
     const product = {
       id,
@@ -31,39 +21,63 @@ class ProductManager {
       code,
       stock,
     };
-    this.products.push(product);
+    products.push(product);
+
+    this.saveProductsToFile(products);
+
+    return product;
   }
 
   getProducts() {
-    return this.products;
+    return this.getProductsFromFile();
   }
 
   getProductById(id) {
-    const product = this.products.find((product) => product.id === id);
+    const products = this.getProductsFromFile();
+    const product = products.find((product) => product.id === id);
 
     if (!product) throw new Error("Producto no encontrado");
 
     return product;
   }
 
-  isExist(key, value) {
-    return this.products.find((product) => product[key] === value);
+  updateProduct(id, updatedFields) {
+    const products = this.getProductsFromFile();
+    const productIndex = products.findIndex((product) => product.id === id);
+
+    if (productIndex === -1) throw new Error("Producto no encontrado");
+
+    const updatedProduct = { ...products[productIndex], ...updatedFields };
+    products[productIndex] = updatedProduct;
+
+    this.saveProductsToFile(products);
+
+    return updatedProduct;
+  }
+
+  deleteProduct(id) {
+    let products = this.getProductsFromFile();
+    const productIndex = products.findIndex((product) => product.id === id);
+
+    if (productIndex === -1) throw new Error("Producto no encontrado");
+
+    products.splice(productIndex, 1);
+
+    this.saveProductsToFile(products);
+  }
+
+  getProductsFromFile() {
+    try {
+      const data = fs.readFileSync(this.path, "utf8");
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
+  }
+
+  saveProductsToFile(products) {
+    fs.writeFileSync(this.path, JSON.stringify(products), "utf8");
   }
 }
 
-const item = {
-  title: "Guitarra Gibson Les Paul Prueba",
-  description: "Guitarra Les Paul Prueba",
-  price: 3000,
-  thumbnail: "No Image",
-  code: "GLP1",
-  stock: 15,
-};
-
-const product = new ProductManager();
-console.log(product.getProducts());
-product.addProduct(item);
-console.log(product.getProducts());
-product.addProduct(item);
-console.log(product.getProductById(1));
-console.log(product.getProductById(2));
+module.exports = ProductManager;
